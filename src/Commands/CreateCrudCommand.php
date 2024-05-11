478f08a844e7158b856e6d7a377f88cd20a2cc23
Michael Vento <michael.vento@everysoft.be>
2024-05-11 15:11:06 +0200

    add composer and command

---
+++ src/Commands/CreateCrudCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace VentoMichael\Crud\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class CreateCrudCommand extends Command
+{
+    protected $signature = 'crud:{model}';
+    protected $description = 'Creates a migration and controller for a model';
+
+    public function handle()
+    {
+        $modelName = Str::studly($this->argument('model'));
+
+        $this->createMigration($modelName);
+        $this->createController($modelName);
+    }
+
+    private function createMigration(string $modelName)
+    {
+        $migrationName = Str::snake($modelName) . '_table';
+        $migrationPath = database_path('migrations/' . $migrationName . '.php');
+
+        if (file_exists($migrationPath)) {
+            $this->error('The migration ' . $migrationName . ' already exists.');
+            return;
+        }
+
+        $this->call('make:migration', ['--name' => $migrationName]);
+
+    }
+
+    private function createController(string $modelName)
+    {
+        $controllerName = $modelName . 'Controller';
+        $controllerPath = app_path('Http/Controllers/' . $controllerName . '.php');
+
+        if (file_exists($controllerPath)) {
+            $this->error('The controller ' . $controllerName . ' already exists.');
+            return;
+        }
+
+        $this->call('make:controller', ['--name' => $controllerName]);
+
+    }
+}
